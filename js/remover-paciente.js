891fc37d39e39563d239f9e6e42607c9b930beaa
Paulo Sergio <paulosergio@infoway-pi.com.br>
2018-07-18 17:48:40 -0300

    Inclusão de efeito fadeout ao excluir item da tabela de pacientes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,5 +2,9 @@ var tabela = document.querySelector("#tabela-pacientes");
 console.log(tabela);
 tabela.addEventListener("dblclick", function(event){
 		console.log(event.target);
-		event.target.parentNode.remove();
+		event.target.parentNode.classList.add("fadeOut");
+		setTimeout(function(){
+			event.target.parentNode.remove();
+		}, 500);
+		
 });
